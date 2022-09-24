67df0cf7bbffc82036ba65bd9affaadb5839d68b
fmurciag <fmurciag@gmail.com>
2022-09-24 13:36:45 +0200

    testado en ubuntu a falta de aceleracion de video

--- grabar.py
+++ grabar.py
@@ -15,28 +15,32 @@ def duracion(h,m,s):
 #indica si se debe continuar grabando
 continuar = True
 #directorio donde se guardaran los videos
-dirVideos ='C:/Users/FRAN/Desktop/Sistema-de-video-continuo/videos'
+dirVideos ='/home/jetnano/Desktop/Dashcam/videos'
 #crear directorio si no existe
 if not os.path.exists(dirVideos):
     os.makedirs(dirVideos)
 
 #iniciamos la grabacion
 while continuar:
-    cam = cv2.VideoCapture(2)
+    cam = cv2.VideoCapture(0)
     #eliminar el ultimo video
     if len(os.listdir(dirVideos)) > 5:
         os.remove(dirVideos+'/'+os.listdir(dirVideos)[0])
     # Inicializar el grabador
-    out=cv2.VideoWriter(dirVideos+'/'+fecha_hora()+'.avi',cv2.VideoWriter_fourcc(*'XVID'),30,(640,480))
+    out=cv2.VideoWriter(dirVideos+'/'+fecha_hora()+'.mp4',cv2.VideoWriter_fourcc(*'mp4v'),30,(1280,720))
     # Grabar durante 5 segundos
-    t_end = time.time() + duracion(0,5,0)
-    while time.time() < t_end:
+    t_end = time.time() + duracion(0,10,0)
+    numFrames=0
+    while (time.time() < t_end):
         ret, frame = cam.read()
+        numFrames+=1
+        print(numFrames)
         cv2.imshow('frame',frame)
         out.write(frame)
         if(cv2.waitKey(1) == ord('q')):
             continuar=False
             break
+        #cv2.waitKey(50)
     # Liberar la cámara y el grabador
     out.release()
     cam.release()
